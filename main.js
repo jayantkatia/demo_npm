e7f432dd650a7d40fe2e8e8bd7d7c38fb4e23ca0
Jayant Katia <jayantkatia65@gmail.com>
2021-11-10 13:42:56 +0530

    feat: add message

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 console.log('Hello World, this is my first npm package')
 console.log('Second version')
 console.log('Third version, if this gets released than semantic-release is smart')
+console.log('Oh so semantic release analyzes commit title... cool')
